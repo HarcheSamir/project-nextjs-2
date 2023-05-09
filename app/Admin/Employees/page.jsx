d4bd09bd1ded8df80fd5fe6ab69f45e158bcebdb
samir <harchesamir007@gmail.com>
2023-05-09 07:03:13 +0100

    a

--- app/Admin/Employees/page.jsx
+++ app/Admin/Employees/page.jsx
@@ -7,6 +7,8 @@ import Image from "next/image";
 import Link from "next/link"
 import axios from "axios"
 import { useRouter } from 'next/navigation';
+import {BsCollectionFill, BsFillCollectionFill  } from 'react-icons/bs'
+
 
 var queryParams = {
   
@@ -97,13 +99,15 @@ export default function Page() {
       </div>
       <div className="flex flex-col w-full  ">
       {accounts .filter((account) => account.email !== 'admin@com').map((account, index) => (
-        <div key={index} onClick={()=>{ router.push(`/Admin/Requests?id=${encodeURIComponent(account.email)}`);}}   className="w-full cursor-pointer   rounded-lg hover:bg-blue-200 hover:scale-[101%] group/item mb-1 px-4 mx-4 relative  mt-1 items-center flex ">
+        <div key={index}    className="w-full cursor-pointer   rounded-lg hover:bg-blue-200 hover:scale-[101%] group/item mb-1 px-4 mx-4 relative  mt-1 items-center flex ">
               <p  className="md:w-[3%]  w-[5%]  text-sm font-bold  text-zinc-700 ">{index +1 }</p>
               <div className="md:w-[30%] w-[50%] flex items-center  text-sm font-bold relative h-16 text-zinc-700 ml-2"><div className="h-[70%] overflow-hidden mr-2  flex-none relative object-cover rounded-md aspect-square"><Image alt="https://static.vecteezy.com/system/resources/previews/001/840/618/original/picture-profile-icon-male-icon-human-or-people-sign-and-symbol-free-vector.jpg" fill className="  mr-2 " src={account.profileImageUrl} /> </div><div className="flex w-full grow  h-full justify-center flex-col "> <p className="text-ellipsis w-[70%] overflow-hidden">{account.name}</p> <p className="text-xs text-zinc-400  text-ellipsis w-[70%] overflow-hidden ">{account.email}</p></div> </div>
               <p className="md:w-[30%] w-[50%]  text-sm font-bold  text-zinc-700  ml-2">{account.job}</p>
               <p className="md:w-[30%] hidden md:block  text-sm font-bold  text-zinc-700 ml-2">{account.phone}</p>
-            <div className="absolute  invisible md:group-hover/item:visible flex justify-evenly top-1/2 -translate-y-1/2 right-[5%]  h-[50%] aspect-[2/1]">
-            <Link href={`/Admin/Employee?id=${encodeURIComponent(account.email)}`}><AiFillEdit className="w-6 hover:scale-150 cursor-pointer h-6 text-green-500"/> </Link>
+            <div className="absolute gap-2  invisible md:group-hover/item:visible flex justify-evenly top-1/2 items-center -translate-y-1/2 right-[5%]  h-[50%] aspect-[2/1]">
+            <Link className="z-50" href={`/Admin/Employee?id=${encodeURIComponent(account.email)}`}><AiFillEdit className="w-7 hover:scale-150 cursor-pointer h-7 text-green-500"/> </Link>
+            <Link className="z-50" href={`/Admin/Requests?id=${encodeURIComponent(account.email)}`}><BsCollectionFill className="w-6  hover:scale-150 cursor-pointer h-6  text-amber-500"/> </Link>
+
               <AiFillDelete className="w-6 h-6 hover:scale-150 cursor-pointer text-red-500"/>
             </div>
             </div>
