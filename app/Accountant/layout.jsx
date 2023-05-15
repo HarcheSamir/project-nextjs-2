f4381029a15ce2bb6a73d181c37fea67a753e0a0
samir <harchesamir007@gmail.com>
2023-05-15 10:47:20 +0100

    a

--- app/Accountant/layout.jsx
+++ app/Accountant/layout.jsx
@@ -115,7 +115,7 @@ export default function Admin({ children }) {
  
   <div className="sm:flex hidden   flex-col w-full bg-[#35465e]   mt-4 ">
   <Link href='/Accountant/Requests'><div className='py-4 hover:bg-[#4b6485]  px-2 flex hover:scale-105  gap-2 items-center font-bold text-xs text-neutral-300'><MdCollectionsBookmark className='h-4 w-4'/> Requests</div></Link>
-  <Link href='/Accountant'><div className='py-4 hover:bg-[#4b6485] px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><RiBillFill className='h-3 w-3'/> Add Trancaction</div></Link>
+  <Link href='/Accountant/AddTransaction'><div className='py-4 hover:bg-[#4b6485] px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><RiBillFill className='h-3 w-3'/> Add Trancaction</div></Link>
 
   <Link href='/Accountant'><div className='py-4 hover:bg-[#4b6485] px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><AiFillFileAdd className='h-4 w-4'/> Add a request</div></Link>
 
@@ -152,7 +152,7 @@ export default function Admin({ children }) {
 
     <div className={ nav ? `flex absolute top-14 sm:hidden animate duration-300   left-0 right-0 z-10 flex-col w-full bg-[#35465e]` : '-translate-y-[200%] flex absolute left-0 right-0 z-10 flex-col w-full bg-[#35465e] animate duration-300'}>
       <Link onClick={()=>{setNav(!nav)}} href='/Accountant/Requests'><div className='py-4 hover:bg-[#4b6485]  px-2 flex hover:scale-105  gap-2 items-center font-bold text-xs text-neutral-300'><BsFillPeopleFill className='h-3 w-3'/> Requests</div></Link>
-      <Link onClick={()=>{setNav(!nav)}} href='/Accountant'><div className='py-4 hover:bg-[#4b6485] z-50  px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><RiBillFill className='h-3 w-3'/> Add Transaction</div></Link>
+      <Link onClick={()=>{setNav(!nav)}} href='/Accountant/AddTransaction'><div className='py-4 hover:bg-[#4b6485] z-50  px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><RiBillFill className='h-3 w-3'/> Add Transaction</div></Link>
 
       <Link onClick={()=>{setNav(!nav)}} href='/Accountant'><div className='py-4 hover:bg-[#4b6485] px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><AiFillFileAdd className='h-4 w-4'/> Add a request</div></Link>
       <Link onClick={()=>{setNav(!nav)}} href='/Accountant'><div className='py-4 hover:bg-[#4b6485] z-50  px-2 flex hover:scale-105 gap-2 items-center font-bold text-xs text-neutral-300'><BsFillCollectionFill className='h-3 w-3'/> Programs</div></Link>
