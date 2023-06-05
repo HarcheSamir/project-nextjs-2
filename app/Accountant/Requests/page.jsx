3448d6660887225a20972f646cd74b34630d89db
samir <harchesamir007@gmail.com>
2023-06-06 00:58:12 +0100

    a

--- app/Accountant/Requests/page.jsx
+++ app/Accountant/Requests/page.jsx
@@ -77,69 +77,6 @@ const [records, setRecords] = useState([]);
 const [pagination, setPagination] = useState({});
 const [loading, setLoading] = useState(false);
 
-useEffect(() => {
-  async function fetchData() {
-    setChartLoading(true);
-    setLoading(true);
-    const { data } = await axios.get("https://server-social-benefits.vercel.app/searchFilter", {
-      params: {
-        for: query,
-        manager_review: 'approved',
-        page: pagination.currentPage || 1,
-        limit: 10,
-      },
-    });
-  
-    const currentYear = new Date().getFullYear();
-    const approvedRequests = data.records.filter((request) => {
-      
-      const isApproved = request.status === 'completed';
-      const isCurrentYear = new Date(request.completedAt).getFullYear() === currentYear;
-      
-      return isApproved && isCurrentYear;
-    });
-    
-    console.log(approvedRequests);
-
-    const totalAmount = approvedRequests.reduce(
-      (sum, request) => sum + parseFloat(request.amount), 0);
-
-    setChartTotal(totalAmount);
-
-
-    const months = Array.from({ length: 12 }, (_, index) => index + 1);
-    const monthlyAmounts = months.map((month) => {
-      const amountForMonth = approvedRequests
-        .filter((request) => new Date(request.completedAt).getMonth() === month - 1)
-        .reduce((sum, request) => sum + parseFloat(request.amount), 0);
-      return amountForMonth;
-    });
-    setChartData([
-      {
-        name: 'Dépenses',
-        data: monthlyAmounts,
-      },
-    ]);
-
-    setRecords(data.records);
-    setPagination(data.infos);
-    setLoading(false);
-    const uniqueTitles = [...new Set(approvedRequests.map((request) => request.service_title))];
-    setChartLabels(uniqueTitles);
-
-    const chartValues = uniqueTitles.map((title) => {
-      const requestsForTitle = data.records.filter((request) => request.service_title === title);
-      const totalAmount = requestsForTitle.reduce((sum, request) => sum + parseFloat(request.amount), 0);
-      const percentage = chartTotal !== 0 ? Math.floor((totalAmount / chartTotal) * 100) : 0;
-      return percentage;
-    });
-    setChartValues(chartValues);
-    setChartLoading(false);
-  }
-
-  fetchData();
-}, [query, pagination.currentPage ,chartTotal]);
-
 
 
   return (
