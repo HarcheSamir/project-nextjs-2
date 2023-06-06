c4f830dacd2ca1dba57981fd66728da8685b0a91
samir <harchesamir007@gmail.com>
2023-06-06 01:04:54 +0100

    a

--- app/Accountant/Requests/page.jsx
+++ app/Accountant/Requests/page.jsx
@@ -1,5 +1,15 @@
 
 'use client'
+import { useRouter } from 'next/navigation';
+import { useState, useEffect} from 'react'
+import React from 'react'
+import axios from 'axios';
+import ReactPaginate from "react-paginate";
+import { FiSearch } from 'react-icons/fi';
+import Image from "next/image";
+import Loading from '@/components/Loading.js';
+import Transactions from '@/components/transactions';
+
 
 export default function Page() {
 
