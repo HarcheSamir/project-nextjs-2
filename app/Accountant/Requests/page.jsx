43b39475e1b675cb6ee2a82331b6ac5bd0f518e5
samir <harchesamir007@gmail.com>
2023-06-06 01:01:59 +0100

    a

--- app/Accountant/Requests/page.jsx
+++ app/Accountant/Requests/page.jsx
@@ -15,47 +15,6 @@ import Transactions from '@/components/transactions';
 
 export default function Page() {
 
-
-function getStatusColor(status) {
-  switch (status) {
-    case 'pending':
-      return 'text-yellow-500'; // Apply yellow color
-    case 'completed':
-      return 'text-green-500'; // Apply green color
-    case 'rejected':
-      return 'text-red-500'; // Apply red color
-    case 'archive':
-      return 'text-blue-500';
-    default:
-      return ''; // No specific color class for other statuses
-  }
-}
-
-//fcts des charts 
-
-const chartRef = useRef(null);
-
-const [chartData, setChartData] = useState([
-  {
-    name: 'Dépenses',
-    data: [0, 200, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0]
-  }
-]);
-
-const [chartLabels, setChartLabels] = useState([]);
-const [chartValues, setChartValues] = useState([null]);
-const [chartTotal, setChartTotal] = useState(0);
-const [chartLoading, setChartLoading] = useState(true);
-
-const router = useRouter();
-
-const [query, setQuery] = useState("");
-const [records, setRecords] = useState([]);
-const [pagination, setPagination] = useState({});
-const [loading, setLoading] = useState(false);
-
-
-
   return (
 
     <div className='h-full w-full'>
